f6620621f8736b72862372f32257e17d02f20336
Enot <natasha.cat982404@gmail.com>
2020-06-02 01:31:42 +0300

    A lil style changes =)

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -6,7 +6,7 @@ function SignUp(props){
     <div className="sign-up-container">
         <form>
             <div className="form-group">
-                <label htmlFor="firstName">First Name</label>
+                <label htmlFor="firstName" className="whiteText">First Name</label>
                 <input  type="text"
                         placeholder="Tom"
                         className="form-control" 
@@ -17,7 +17,7 @@ function SignUp(props){
                 <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div className="form-group">
-                <label htmlFor="secondName">Second Name</label>
+                <label htmlFor="secondName" className="whiteText">Second Name</label>
                 <input  type="text"
                         placeholder="Jerry"
                         className="form-control"
@@ -27,23 +27,26 @@ function SignUp(props){
                         onChange={e => props.changeInput(e.target.value, e.target.name)}></input>
             </div>
             <div className="form-group">
-                <label htmlFor="email">Email address</label>
+                <label htmlFor="email" className="whiteText">Email address</label>
                 <input  type="text"
                         placeholder="tom123@gmail.com"
                         className="form-control"
                         id="email" 
                         name="email"
                         value={props.email}
-                        onChange={e => props.changeInput(e.target.value, e.target.name)}></input>
+                        onChange={e => props.changeInput(e.target.value, e.target.name)}/>
             </div>
             <div className="form-group form-check">
-                <label className="form-check-label" htmlFor="password">Password</label>
+                <label className="form-check-label whiteText" htmlFor="password">Password</label>
                 <input type="password" className="form-control" id="password"
                        name="password"
                        value={props.password}/>
-                    
             </div>
-            <button type="submit" className="btn btn-primary">Continue</button>
+            <button type="submit" className="btn btn-info">Continue</button>
+            <hr className="hr-line"/>
+            <div className="form-group">
+                <a href="#" className="link">Already have an account? Log In</a>
+            </div>
         </form>
     </div>
     )
